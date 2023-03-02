47c64727d420eab3d3c12791b9f26bba1379b276
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-03-02 15:04:48 +0100

    tables styles almost done

--- src/main/frontend/src/components/ConcertList.vue
+++ src/main/frontend/src/components/ConcertList.vue
@@ -21,77 +21,67 @@ const columns = [
 
 const rows = [
   {
-    imagen: "https://cdn.quasar.dev/img/avatar.png",
-    Events: 'Frozen Yogurt',
-    pic: 159,
-    Description: 6.0,
+    imagen: "/src/assets/images/Altj.jpg",
+    Events: 'Alt-J',
+    Description: 'vfvef efvefvfrefrv efvwrefvervfe fvefv',
     Date: 24,
     MaxCrowd: 4.0,
     imageIndex: 0
   },
   {
-    imagen: "https://i0.wp.com/losmejoresrock.com/wp-content/uploads/2022/12/bon-jovi-1992.jpg?fit=561%2C324&ssl=1",
-    Events: 'Ice cream sandwich',
-    pic: 237,
-    Description: 9.0,
+    imagen: "/src/assets/images/bon-jovi-1992.PNG",
+    Events: 'Bon Jovi',
+    Description: 'vfvef efvefvfrefrv efvwrefvervfe vfrf',
     Date: 37,
     MaxCrowd: 4.3
   },
   {
-    imagen: "/src/assets/images/ALT-J.jpg",
-    Events: 'Eclair',
-    pic: 262,
-    Description: 16.0,
+    imagen: "/src/assets/images/king-gizzard-and-the-lizard-wizard-2022.jpg",
+    Events: 'King Gizzard & The Lizard Wizard',
+    Description: 'vfvef efvefvfrefrv efvwrefv fvefv vfrf',
     Date: 23,
     MaxCrowd: 6.0
   },
   {
-    Events: 'Cupcake',
-    pic: 305,
-    imageIndex: 0,
+    imagen: "/src/assets/images/Los-Gatos-De-Chernobyl.jpg",
+    Events: 'Los Gatos De Chernobyl',
     Description: 3.7,
     Date: 67,
     MaxCrowd: 4.3
   },
   {
-    imageIndex: 1,
-    Events: 'Gingerbread',
-    pic: 356,
+    imagen: "/src/assets/images/TashSultana.jpeg",
+    Events: 'Tash Sultana',
     Description: 16.0,
     Date: 49,
     MaxCrowd: 3.9
   },
   {
     Events: 'Jelly bean',
-    pic: 375,
     Description: 0.0,
     Date: 94,
     MaxCrowd: 0.0
   },
   {
     Events: 'Lollipop',
-    pic: 392,
     Description: 0.2,
     Date: 98,
     MaxCrowd: 0
   },
   {
     Events: 'Honeycomb',
-    pic: 408,
     Description: 3.2,
     Date: 87,
     MaxCrowd: 6.5
   },
   {
     Events: 'Donut',
-    pic: 452,
     Description: 25.0,
     Date: 51,
     MaxCrowd: 4.9
   },
   {
     Events: 'KitKat',
-    pic: 518,
     Description: 26.0,
     Date: 65,
     MaxCrowd: 7
@@ -112,14 +102,15 @@ export default {
 <template>
   <div class="q-pa-md">
     <q-table class="bg-grey-4" :rows="rows" :columns="columns" row-key="name" selection="multiple"
-      v-model:selected="selected">
+      v-model:selected="selected" :grid="$q.screen.xs">
+
 
       <template #body-cell-imagen="{ row }">
         <q-img :src="row.imagen" />
       </template>
 
       <template #body-cell-Button>
-        <q-td align="center" >
+        <q-td align="center">
           <q-btn push color="white" text-color="primary" label="Add" />
           <q-btn push color="white" text-color="primary" label="Edit" />
           <q-btn push color="red" text-color="primary" label="delete" />
@@ -132,34 +123,54 @@ export default {
 
       <template v-slot:body-selection="scope">
         <q-toggle v-model="scope.selected" />
-
       </template>
 
     </q-table>
   </div>
 </template>
 <style lang="scss">
-// q-table{
-//   display: flex;
-
-// }
 .q-pa-md {
   width: 80vw;
+
+  .q-table {
+    thead {
+      background-color: #D9D9D9;
+    }
+
+    th {
+      font-size: 2vw;
+    }
+
+    tbody {
+      background-color: #B7B7B7;
+
+      td {
+        &:nth-child(2) {
+          background-color: #9E0404;
+          color: white;
+          max-width: 3vw;
+          white-space: pre-wrap;
+        }
+
+        &:nth-child(4) {
+          background-color: #9E0404;
+          white-space: pre-wrap;
+          max-width: 20vw;
+        }
+        
+        font-size: 1.5vw;
+        font-weight: bold;
+      }
+    }
+  }
+  
+  .q-table__bottom {
+    background-color: white;
+  }
 }
 
-td:nth-child(2) {
-  background-color: #9E0404;
-}
-
-thead {
-  background-color: #D9D9D9;
-}
-
-tbody {
-  background-color: #B7B7B7;
-}
-
-.q-table__bottom {
-  background-color: white;
+.q-img {
+  height: 15vh;
+  width: 15vw;
 }
 </style>
